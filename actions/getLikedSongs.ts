77f21fe4fd8e488055e5abbda25598e54546d368
Chu <dzhu2409@gmail.com>
2024-06-14 23:02:02 -0500

    Favorites funcionality

---
+++ actions/getLikedSongs.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import { Song } from '@/types'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+const getLikedSongs = async (): Promise<Song[]> => {
+  const supabase = createServerComponentClient({
+    cookies,
+  })
+
+  const {
+    data: { user },
+    error: sessionError,
+  } = await supabase.auth.getUser()
+  if (sessionError) {
+    console.log(sessionError)
+    return []
+  }
+
+  const { data, error } = await supabase
+    .from('liked_songs')
+    .select('*, songs(*)')
+    .eq('user_id', user!.id)
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.log(error)
+    return []
+  }
+
+  if (!data) return []
+
+  return data.map((item) => ({
+    ...item.songs,
+  }))
+}
+
+export default getLikedSongs
---
+++ components/LikeButton.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { useAuthModal, useUser } from '@/hooks'
+import { useSessionContext } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+
+interface LikeButtonProps {
+  songId: string
+}
+
+const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
+  const router = useRouter()
+  const { supabaseClient } = useSessionContext()
+
+  const { onOpen } = useAuthModal()
+  const { user } = useUser()
+
+  const [isLiked, setIsLiked] = useState(false)
+
+  useEffect(() => {
+    if (!user?.id) return
+
+    const fetchData = async () => {
+      const { data, error } = await supabaseClient
+        .from('liked_songs')
+        .select('*')
+        .eq('user_id', user.id)
+        .eq('song_id', songId)
+        .single()
+
+      if (!error && data) {
+        setIsLiked(true)
+      }
+    }
+
+    fetchData()
+  }, [songId, supabaseClient, user?.id])
+
+  const Icon = isLiked ? AiFillHeart : AiOutlineHeart
+
+  const handleLike = async () => {
+    if (!user) return onOpen()
+
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from('liked_songs')
+        .delete()
+        .eq('user_id', user.id)
+        .eq('song_id', songId)
+
+      if (error) {
+        toast.error('Failed to unlike song')
+      } else {
+        setIsLiked(false)
+        toast.success('Song unliked')
+      }
+    } else {
+      const { error } = await supabaseClient
+        .from('liked_songs')
+        .insert([{ user_id: user.id, song_id: songId }])
+
+      if (error) {
+        toast.error('Failed to like song')
+      } else {
+        setIsLiked(true)
+        toast.success('Song liked')
+      }
+    }
+
+    router.refresh()
+  }
+
+  return (
+    <button className='hover:opacity-75 transition' onClick={handleLike}>
+      <Icon color={isLiked ? '#22C55E' : 'white'} />
+    </button>
+  )
+}
+
+export default LikeButton
